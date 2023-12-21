7a3ff00f658ed49a3af3347eee851d5c1a39ae1b
Carlos Breininger <86577114+cbreininger@users.noreply.github.com>
2023-12-21 13:55:03 -0800

    Azure App Gw audit

---
+++ auditApplicationGateway.ps1
@@ -0,0 +1,27 @@
+﻿param (
+    [Parameter(mandatory=$true)]
+    [String]$subscription,
+    [Parameter(mandatory=$true)]
+    [String]$vnet
+)
+Connect-AzAccount
+Select-AzSubscription -Subscription $subscription
+$appGws=Get-AzApplicationGateway
+foreach ($appGw in $appGws) {
+    $bkPools=$appGw.BackendAddressPools
+    foreach ($bkPool in $bkPools) {
+        $addresses=$bkPool.BackendAddresses
+        foreach ($address in $addresses) {
+            $IPTest=Test-AzPrivateIPAddressAvailability -ResourceGroupName $appGw.ResourceGroupName -VirtualNetworkName $vnet -IPAddress $address.IpAddress
+            if ($IPTest.Available) {
+                $action="REMOVE"
+                $color = "RED"
+            } else { 
+                $action="ACTIVE" 
+                $color = "GREEN"
+            }
+            write-host "$($appGw.Name) - $($bkPool.Name) - $($address.IpAddress) - $($action) " -ForegroundColor $color
+        }
+    Write-Host "======================================================================================================================"
+    }
+}
