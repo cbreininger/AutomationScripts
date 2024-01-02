9013f1bae270f6d056d9f0f8ac39fc289708cd6a
Carlos Breininger <86577114+cbreininger@users.noreply.github.com>
2024-01-02 10:36:25 -0800

    Create schedTaskEnabler
    Script to enable a scheduled task from a server list

---
+++ ScheduledTaskScripts/schedTaskEnabler.ps1
@@ -0,0 +1,12 @@
+﻿$taskName = #Add task name here
+$servers = @(
+  #Add server list sepparated by comma 
+)
+foreach ($server in $servers) {
+    $schedBounce = Get-ScheduledTask -CimSession $Server -TaskName $taskName
+    if ($schedBounce.State -eq "Disabled") {
+        Enable-ScheduledTask -CimSession $server -TaskName $taskName
+        $schedBounce = Get-ScheduledTask -CimSession $Server -TaskName $taskName
+        Write-Host "Server: $($server) ---> $($schedBounce.State)"
+      }
+}
