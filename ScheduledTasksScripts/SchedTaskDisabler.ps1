6f6919e481101c142feaa2018948a5ff9bc77251
Carlos Breininger <86577114+cbreininger@users.noreply.github.com>
2024-01-02 10:17:38 -0800

    Create SchedTaskDisabler
    Script to disable a scheduled task from a server list

---
+++ ScheduledTasksScripts/SchedTaskDisabler.ps1
@@ -0,0 +1,12 @@
+﻿$taskName = #Add task name here
+$servers = @(
+  # Add server list sepparated by comma
+)
+foreach ($server in $servers) {
+    $schedBounce = Get-ScheduledTask -CimSession $Server -TaskName $taskName
+    if ($schedBounce -eq "Ready") {
+        Disable-ScheduledTask -CimSession $server -TaskName $taskName
+        $schedBounce = Get-ScheduledTask -CimSession $Server -TaskName $taskName
+        Write-Host "Server: $($server) ---> $($schedBounce.State)"
+      }
+}
